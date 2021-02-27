01543803e8ddc8d56f3631a42a3c6cadeb646023
Aphrodite <72478102+onomatopoetica@users.noreply.github.com>
2021-02-27 14:01:17 -0700

    Added exercise.js entries.js journal.js in public

---
+++ public/js/entries.js
@@ -0,0 +1,137 @@
+$(document).ready(function () {
+    // entriesContainer holds all of our posts
+    var entriesContainer = $(".entries-container");
+    var postCategorySelect = $("#category");
+    // Click events for the edit and delete buttons
+    $(document).on("click", "button.delete", handlePostDelete);
+    $(document).on("click", "button.edit", handlePostEdit);
+    // Variable to hold our posts
+    var posts;
+
+    // The code below handles the case where we want to get entries for a specific exercise
+    // Looks for a query param in the url for exercise_id
+    var url = window.location.search;
+    var exerciseId;
+    if (url.indexOf("?exercise_id=") !== -1) {
+        exerciseId = url.split("=")[1];
+        getPosts(exerciseId);
+    }
+    // If there's no exerciseId we just get all entries as usual
+    else {
+        getPosts();
+    }
+
+    // This function grabs posts from the database and updates the view
+    function getPosts(exercise) {
+        exerciseId = exercise || "";
+        if (exerciseId) {
+            exerciseId = "/?exercise_id=" + exerciseId;
+        }
+        $.get("/api/posts" + exerciseId, function (data) {
+            console.log("Posts", data);
+            posts = data;
+            if (!posts || !posts.length) {
+                displayEmpty(exercise);
+            }
+            else {
+                initializeRows();
+            }
+        });
+    }
+
+    // This function does an API call to delete posts
+    function deletePost(id) {
+        $.ajax({
+            method: "DELETE",
+            url: "/api/posts/" + id
+        })
+            .then(function () {
+                getPosts(postCategorySelect.val());
+            });
+    }
+
+    // InitializeRows handles appending all of our constructed entries HTML inside entriesContainer
+    function initializeRows() {
+        entriesContainer.empty();
+        var postsToAdd = [];
+        for (var i = 0; i < posts.length; i++) {
+            postsToAdd.push(createNewRow(posts[i]));
+        }
+        entriesContainer.append(postsToAdd);
+    }
+
+    // This function constructs an entry's HTML
+    function createNewRow(post) {
+        var formattedDate = new Date(post.createdAt).toLocaleDateString();
+        var newPostCard = $("<div>");
+        newPostCard.addClass("card");
+        var newPostCardHeading = $("<div>");
+        newPostCardHeading.addClass("card-header");
+        var deleteBtn = $("<button>");
+        deleteBtn.text("x");
+        deleteBtn.addClass("delete btn btn-danger");
+        var editBtn = $("<button>");
+        editBtn.text("EDIT");
+        editBtn.addClass("edit btn btn-info");
+        var newPostTitle = $("<h2>");
+        var newPostDate = $("<small>");
+        var newPostExercise = $("<h5>");
+        newPostExercise.text("Exercise Category: " + post.Exercise.name);
+        newPostAuthor.css({
+            float: "right",
+            color: "blue",
+            "margin-top":
+                "-10px"
+        });
+        var newPostCardBody = $("<div>");
+        newPostCardBody.addClass("card-body");
+        var newPostBody = $("<p>");
+        newPostTitle.text(post.title + " ");
+        newPostBody.text(post.body);
+        newPostDate.text(formattedDate);
+        newPostTitle.append(newPostDate);
+        newPostCardHeading.append(deleteBtn);
+        newPostCardHeading.append(editBtn);
+        newPostCardHeading.append(newPostTitle);
+        newPostCardHeading.append(newPostAuthor);
+        newPostCardBody.append(newPostBody);
+        newPostCard.append(newPostCardHeading);
+        newPostCard.append(newPostCardBody);
+        newPostCard.data("post", post);
+        return newPostCard;
+    }
+
+    // This function figures out which entry we want to delete and then calls deletePost
+    function handlePostDelete() {
+        var currentPost = $(this)
+            .parent()
+            .parent()
+            .data("post");
+        deletePost(currentPost.id);
+    }
+
+    // This function figures out which post we want to edit and takes it to the appropriate url
+    function handlePostEdit() {
+        var currentPost = $(this)
+            .parent()
+            .parent()
+            .data("post");
+        window.location.href = "/journal?post_id=" + currentPost.id;
+    }
+
+    // This function displays a message when there are no posts
+    function displayEmpty(id) {
+        var query = window.location.search;
+        var partial = "";
+        if (id) {
+            partial = " for Exercise #" + id;
+        }
+        entriesContainer.empty();
+        var messageH2 = $("<h2>");
+        messageH2.css({ "text-align": "center", "margin-top": "50px" });
+        messageH2.html("No entries yet" + partial + ", navigate <a href='/journal" + query +
+            "'>here</a> in order to get started.");
+        entriesContainer.append(messageH2);
+    }
+
+});
---
+++ public/js/exercise.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+    // Getting references to the name input and exercise container, as well as the table body
+    var nameInput = $("#exercise-name");
+    var exerciseList = $("tbody");
+    var exerciseContainer = $(".exercise-container");
+    // Adding event listeners to the form to create a new object, and the button to delete
+    // an Exercise
+    $(document).on("submit", "#exercise-form", handleExerciseFormSubmit);
+    $(document).on("click", ".delete-exercise", handleDeleteButtonPress);
+
+    // Getting the initial list of Exercises
+    getExercises();
+
+    // A function to handle what happens when the form is submitted to create a new Exercise
+    function handleExerciseFormSubmit(event) {
+        event.preventDefault();
+        // Don't do anything if the name fields hasn't been filled out
+        if (!nameInput.val().trim().trim()) {
+            return;
+        }
+        // Calling the upsertExercise function and passing in the value of the name input
+        upsertExercise({
+            name: nameInput
+                .val()
+                .trim()
+        });
+    }
+
+    // A function for creating an exercise. Calls getExercises upon completion
+    function upsertExercise(exerciseData) {
+        $.post("/api/exercises", exerciseData)
+            .then(getExercises);
+    }
+
+    // Function for creating a new list row for exercises
+    function createExerciseRow(exerciseData) {
+        var newTr = $("<tr>");
+        newTr.data("exercise", exerciseData);
+        newTr.append("<td>" + exerciseData.name + "</td>");
+        if (exerciseData.Posts) {
+            newTr.append("<td> " + exerciseData.Posts.length + "</td>");
+        } else {
+            newTr.append("<td>0</td>");
+        }
+        newTr.append("<td><a href='/entries?exercise_id=" + exerciseData.id + "'>Go to Posts</a></td>");
+        newTr.append("<td><a href='/journal?exercise_id=" + authorData.id + "'>Create a Post</a></td>");
+        newTr.append("<td><a style='cursor:pointer;color:red' class='delete-exercise'>Delete Exercise</a></td>");
+        return newTr;
+    }
+
+    // Function for retrieving exercises and getting them ready to be rendered to the page
+    function getExercises() {
+        $.get("/api/exercises", function (data) {
+            var rowsToAdd = [];
+            for (var i = 0; i < data.length; i++) {
+                rowsToAdd.push(createExerciseRow(data[i]));
+            }
+            renderExerciseList(rowsToAdd);
+            nameInput.val("");
+        });
+    }
+
+    // A function for rendering the list of exercises to the page
+    function renderExerciseList(rows) {
+        exerciseList.children().not(":last").remove();
+        exerciseContainer.children(".alert").remove();
+        if (rows.length) {
+            console.log(rows);
+            exerciseList.prepend(rows);
+        }
+        else {
+            renderEmpty();
+        }
+    }
+
+    // Function for handling what to render when there are no exercises
+    function renderEmpty() {
+        var alertDiv = $("<div>");
+        alertDiv.addClass("alert alert-danger");
+        alertDiv.text("You must create an Exercise before you can create a Post.");
+        exerciseContainer.append(alertDiv);
+    }
+
+    // Function for handling what happens when the delete button is pressed
+    function handleDeleteButtonPress() {
+        var listItemData = $(this).parent("td").parent("tr").data("exercise");
+        var id = listItemData.id;
+        $.ajax({
+            method: "DELETE",
+            url: "/api/exercises/" + id
+        })
+            .then(getExercises);
+    }
+});
