f4d1dab4b432749b5668fb6c145ffeda761d0db8
Aphrodite <72478102+onomatopoetica@users.noreply.github.com>
2021-03-01 19:35:10 -0700

    Added package.json + fxd relative links

--- models/exercise.js
+++ models/exercise.js
@@ -1,9 +1,9 @@
-module.export = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Exercise = sequelize.define("Exercise", {
         name: DataTypes.STRING
     });
 
-    Exercise.associate = function(models) {
+    Exercise.associate = function (models) {
         Exercise.hasMany(models.Post, {
             onDelete: "cascade"
         });
