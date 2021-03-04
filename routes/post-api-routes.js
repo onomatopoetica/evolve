915c32ab0c873ba87e5c2a4d787ed942c9a1b75b
Aphrodite <72478102+onomatopoetica@users.noreply.github.com>
2021-03-04 09:55:44 -0700

    README + fxd links in exercise.html

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -15,16 +15,14 @@ module.exports = function (app) {
     // GET route for getting all of the posts
     app.get("/api/posts", function (req, res) {
         var query = {};
-        // ??? NEED TO CHANGE TO exercise_id ???
         if (req.query.exercise_id) {
             query.ExerciseId = req.query.exercise_id;
         }
         // Here we add an "include" property to our options in our findAll query
         // We set the value to an array of the models we want to include in a left outer join
-        // In this case, just db.Author
+        // In this case, just db.Exercise
         db.Post.findAll({
             where: query,
-            // ??? db.Exercise ???
             include: [db.Exercise]
         }).then(function (dbPost) {
             res.json(dbPost);
